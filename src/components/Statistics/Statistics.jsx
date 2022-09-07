53210a550613ef4fe2b60681a264f989400e00e9
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-09-07 18:14:25 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,7 +7,7 @@ function getRandomHexColor() {
 export default function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
-      {title && <h2 className={css.title}>Upload stats</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => (
           <li
